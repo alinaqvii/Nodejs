c034c5f43dd1b777c13ed5ce63388b4d0c0d76a7
alinaqvii <wajahat.naqvi21@gmail.com>
2019-02-02 16:54:57 +0500

    Nodejs

---
+++ Exercise Files/Ch05/05_08/start/promptPerson.js
@@ -0,0 +1,34 @@
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+var fs = require("fs");
+
+var realPerson = {
+	name: '',
+	sayings: []
+};
+
+
+rl.question("What is the name of a real person? ", function(answer) {
+
+	realPerson.name = answer;
+
+	//
+	//	TODO: Use a Writable Stream
+	//
+
+
+		//
+		//TODO: Write to the stream
+		//
+		
+	});
+
+});
+
+
+rl.on('close', function() {
+
+	console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
+	process.exit();
+
+});
