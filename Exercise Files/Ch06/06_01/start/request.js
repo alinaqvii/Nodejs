b2fe0fbc81e95deed1b93cfdeb4ca4d2fb4c2b1a
alinaqvii <wajahat.naqvi21@gmail.com>
2019-02-06 00:16:46 +0500

    Making a HTTPS request

---
+++ Exercise Files/Ch06/06_01/start/request.js
@@ -0,0 +1,47 @@
+var https = require('https');
+
+var fs = require('fs');
+
+var option = {
+    hostname : 'en.wikipedia.org',
+    port: 443,
+    path:'/wiki/Nelson_Mandela',
+    method: 'GET'
+};
+
+var req = https.request(option, function(res){
+
+    var responseBody = '';
+
+    console.log('Response from server started');
+    console.log(`Server Status: ${res.statusCode} `);
+    console.log('Response Header: %j', res.headers);
+
+    res.setEncoding('UTF-8');
+
+    res.once('data', function(chunk){
+        console.log(chunk);
+    });
+
+    res.on('data', function(chunk){
+        console.log(`--chunk-- ${chunk.length}`);
+        responseBody += chunk;
+    });
+
+    res.on('end', function(){
+        fs.writeFile('nelson-mandela.html', responseBody, function(err){
+            if(err){
+                throw err;
+            }
+
+            console.log('File Downloaded');
+        });
+    });
+
+});
+
+req.on('error', function(err){
+    console.log(`problem with request: ${err.message}`);
+});
+
+req.end();
