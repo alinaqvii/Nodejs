75eb70c8f0b744fb5e8fd4109bd4ec95f6553841
alinaqvii <wajahat.naqvi21@gmail.com>
2019-08-11 00:34:20 +0500

    Node JS Day 1

--- Exercise Files/NodeCrashCourse/path_demo.js
+++ Exercise Files/NodeCrashCourse/path_demo.js
@@ -8,23 +8,9 @@ console.log(path.basename(__dirname));
 
 // File Extension
 console.log(path.extname(__filename));
+
 // Create path object
 console.log(path.basename(__filename).base);
 
-//To require external files in JS
-const Person = require('./person');
-
-
-
-
-//To Print in JS
-// console.log(person);
-// console.log(person.name)
-
-
-const person1 = new Person('Ali Naqvi', 29);
-
-
-person1.greeting();
-//Base file name
-console.log(__filename);
+// Concatenate Path
+console.log(path.join(__dirname, 'test', 'hello.html'));
